9cd355e141d78d3f3823933947229c3a38353bcf
coco723 <1352118502@qq.com>
2022-08-09 16:16:39 +0800

    feat: 代码提交测试

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m "deploy"
 
 # 如果发布到https://<USERNAME>.github.io<REPO>
-git push -f git@github.com:cnstrong-tech/blog.git master:gh-pages
+git push -f git@github.com-cnstrong-tech:cnstrong-tech/blog.git master:gh-pages
 
 cd -
